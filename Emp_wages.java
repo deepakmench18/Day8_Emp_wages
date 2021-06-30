1003449460da9b8166d2c7e5fe0b27c4b094414a
deepakmench18 <80206715+deepakmench18@users.noreply.github.com>
2021-06-30 10:15:52 +0530

    Emp_wages

---
+++ Emp_wages.java
@@ -0,0 +1,12 @@
+package com.bridgelab.program;
+import com.bridgelab.program.M
+public class Emp_wages {
+
+	public static void main(String[] args) {
+		Main Obj = new Main();
+		Obj.IsPresent(0, 0, 0, 0);
+		
+
+	}
+
+}
